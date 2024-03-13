027e42cd5dcd483f6d4b1e89ade5d96e1ae72ce5
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2024-03-13 12:21:51 +0000

    // check if the number of parameters is correct

--- src/5g_auth_platform.c
+++ src/5g_auth_platform.c
@@ -2,8 +2,16 @@
 // João Maria Pereira Carvalho de Picado Santos 2022213725
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
+    // check if the number of parameters is correct
+    if (argc != 2)
+    {
+        printf("Error: Wrong parameters, use ./5g_auth_platform {config-file}\n");
+        exit(1);
+    }
+    
     return 0;
 }
--- src/mobile_user.c
+++ src/mobile_user.c
@@ -2,8 +2,16 @@
 // João Maria Pereira Carvalho de Picado Santos 2022213725
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
+    // check if the number of parameters is correct
+    if (argc != 7)
+    {
+        printf("Error: Wrong parameters, use ./mobile_user {plafond inicial} {número de pedidos de autorização} {intervalo VIDEO} {intervalo MUSIC} {intervalo SOCIAL} {dados a reservar}\n");
+        exit(1);
+    }
+
     return 0;
 }
