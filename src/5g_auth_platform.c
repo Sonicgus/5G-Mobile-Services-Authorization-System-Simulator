ec26103b62eb59ea4bd34eeb8d96442a95d9ad43
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2024-03-13 12:07:23 +0000

    add main

--- src/5g_auth_platform.c
+++ src/5g_auth_platform.c
@@ -1,2 +1,9 @@
 // Gustavo Samuel de Alves e Bastos de André e Lima 2020217743
 // João Maria Pereira Carvalho de Picado Santos 2022213725
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
