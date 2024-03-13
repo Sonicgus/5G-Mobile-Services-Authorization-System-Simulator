5eb5841407a8aa639a80b5fcce6ec5d944888689
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2024-03-13 12:52:10 +0000

    create Authorization Request Manager and Monitor Engine processes

--- src/mobile_user.c
+++ src/mobile_user.c
@@ -3,11 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 // #define DEBUG //remove this line to remove debug messages
 #define LOG_FILENAME "log.txt"
 
 FILE *log_fp;
+pid_t system_manager_pid, authorization_request_manager_pid, monitor_engine_pid;
 
 void start(const char *config_file)
 {
@@ -19,7 +22,6 @@ void start(const char *config_file)
         perror("Error: opening log file");
         exit(1);
     }
-
 }
 
 void finish()
@@ -27,6 +29,46 @@ void finish()
     // close log file
     if (log_fp)
         fclose(log_fp);
+}
+
+void authorization_request_manager()
+{
+    exit(0);
+}
+
+void monitor_engine()
+{
+    exit(0);
+}
+
+void system_manager()
+{
+    system_manager_pid = getpid();
+
+    // create the alerts watcher process
+    authorization_request_manager_pid = fork();
+
+    if (authorization_request_manager_pid < 0)
+    {
+        perror("Error: creating process Authorization Request Manager\n");
+        return;
+    }
+    else if (authorization_request_manager_pid == 0)
+    {
+        authorization_request_manager();
+    }
+    
+    monitor_engine_pid = fork();
+
+    if (monitor_engine_pid < 0)
+    {
+        perror("Error: creating process Monitor Engine\n");
+        return;
+    }
+    else if (monitor_engine_pid == 0)
+    {
+        monitor_engine();
+    }
 }
 
 int main(int argc, char *argv[])
@@ -38,8 +80,12 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    // start the simulation
     start(argv[1]);
 
+    system_manager();
+
+    // finish the simulation
     finish();
 
     exit(0);
